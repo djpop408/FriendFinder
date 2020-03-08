595ad130c4930311048ed48886e4e7243f95163f
Anthony Nguyen <anthonynguyen@anthonys-mbp.lan>
2020-03-07 23:55:43 -0800

    initial setup of all files and packages

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+    {
+      name: "Alfred",
+      photo: "http://image.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+        ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
